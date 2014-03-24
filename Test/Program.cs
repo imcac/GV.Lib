ef49b4d23fd00ab02cbaa46f048589d5561b128a
Questionfeet <ljn1@qq.com>
2014-03-25 00:14:54 +0800

    GV.lib Dynamic execute Web Service Method , and return execute Result

---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GV.Lib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(
+            DynamicWS.GetServiceResultByUrlAndMethod("http://iom.unioncloud.com/ws/IOM_WebService.asmx", "IOM_Role_GetMemberListByRoleIdForCount",
+                "strJson=[{\"IOM_Version\": \"Web3.0\"},{\"roleId\": \"4e5fba85-c602-4e80-95be-b52f9bf117c6\",\"pageSize\":\"10\",\"pageIndex\":\"1\",\"serviceId\": \"IBD101\"}]"));
+
+            Console.Read();
+        }
+    }
+}
